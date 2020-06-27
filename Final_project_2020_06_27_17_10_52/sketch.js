7e05461525523787611fd09831366a6fa346c6a2
BenjiDuke <54375202+BenjiDuke@users.noreply.github.com>
2020-06-27 13:13:57 -0400

    Add files via upload
    final project on the p5js web editor, work in progress

---
+++ Final_project_2020_06_27_17_10_52/sketch.js
@@ -0,0 +1,153 @@
+/* CITA-120 Final Project
+ * Authors: Benji Duke, Henry Horlbeck
+ * This project shows an animation with a beginning, 
+ * middle, and end. The animation uses sprites via the
+ * ImgSprite and SpriteImageMapper classes. The animation is
+ * put together by having each scene separated into functions
+ * that are called in the draw function based on the current
+ * frame number, tracked by the variable frameCounter.
+*/
+
+let canvasSize;
+// Declare background images
+let imgBackground1;
+let imgBackground2;
+// Camera that will view our 3D scene.
+let cam1;
+// Declare frame count to keep animation on track.
+let frameCounter;
+// Position variables for the camera.
+let zPos;
+let xPos;
+// Declare sprite image and mapper.
+// ImgSprite object
+let wolfSprite;
+// Image
+let wolfSpriteSheet;
+// SpriteSheetMapper object
+let wolfSpriteMapper;
+
+function preload() {
+	
+  // Load an image that you uploaded into your P5JS WebEditor assets folder.
+  wolfSpriteSheet = loadImage("wolf_image_flip2.png");
+  imgBackground1 = loadImage("storefront.png");
+  imgBackground2 = loadImage("fullbar.png");
+}
+
+function setup() {
+  canvasSize = 500;
+  // Add argument WEBGL so we get a 3D drawing canvas.
+  createCanvas(canvasSize,canvasSize, WEBGL);
+	
+  // Initialize camera that views our 3D scene.
+  cam1 = createCamera();
+
+  // 60 - field of view angle in degrees - this is our lens, wide or narrow.
+  // width / height sets aspect ratio so the 3D rendering fits the shape of our sketch window.
+  // 0.1 is the nearest distance that we expect any object to be to our camera.
+  // 1000 is the farthest distance that we expect any object to be to our camera.
+  // Any object too close or too far will be clipped (excluded) from what we see.
+  cam1.perspective(60, width/height, 0.1, 1000);
+  // Set position of the camera in 3D.
+  cam1.setPosition(0,0,80);
+  // Aim the camera at this specified point.
+  cam1.lookAt(0,0,0);
+  frameCounter = 0;
+  zPos = 80;
+  xPos = 0;
+  wolfSpriteMapper = new SpriteSheetMapper(wolfSpriteSheet, 6, 3);
+  wolfSpriteMapper.setAction(0);
+  wolfSprite = new ImgSprite("wolf", wolfSpriteMapper, 300, 384);
+  frameRate(60);
+}
+
+function draw() {
+  if (frameCounter < 240){
+    scene1();
+    zPos-=0.1;
+    cam1.setPosition(0,0,zPos);
+  }
+  else if (frameCounter < 1600)
+    scene2();
+  else if (frameCounter < 3600){
+    scene3();
+  }
+  else
+    background(220);
+  frameCounter++;
+}
+
+function scene1(){
+    background(220);
+  
+  // Replace fill command with command that specifies current texture image.
+  texture(imgBackground1);
+	
+  // Draw a square.
+  let side = 500;
+  beginShape();
+	
+  // top-left corner of polygon.
+  // Map image pixel with (U,V) of (0,0) to top-left vertex of polygon.
+  vertex(-side,side,  0,0);
+	
+  // bottom-left corner
+  // Map image pixel with (U,V) of (0,imgBackground.height) to bottom-left vertex of polygon.
+  vertex(-side,-side, 0,imgBackground1.height);
+	
+  // bottom-right corner
+  // Map image pixel with (U,V) of (imgBackground.width,imgBackground.height) to bottom-right vertex of polygon.
+  vertex(side,-side,  imgBackground1.width,imgBackground1.height);
+	
+  // top-right corner
+  // Map image pixel with (U,V) of (imgBackground.width,0) to top-right vertex of polygon.
+  vertex(side,side,  imgBackground1.width,0);
+
+  endShape(CLOSE);
+}
+
+function scene2(){
+  background(220);
+  cam1.setPosition(xPos,0,-60);
+  cam1.lookAt(xPos,0,0);
+  if(xPos < 620)
+    xPos+=2;
+  // Replace fill command with command that specifies current texture image.
+  texture(imgBackground2);
+  let sideX = 1024;
+  let sideY = 512;
+  
+  beginShape();
+	
+  // top-left corner of polygon.
+  // Map image pixel with (U,V) of (0,0) to top-left vertex of polygon.
+  vertex(-sideX,sideY,  0,0);
+	
+  // bottom-left corner
+  // Map image pixel with (U,V) of (0,imgBackground.height) to bottom-left vertex of polygon.
+  vertex(-sideX,-sideY, 0,imgBackground2.height);
+	
+  // bottom-right corner
+  // Map image pixel with (U,V) of (imgBackground.width,imgBackground.height) to bottom-right vertex of polygon.
+  vertex(sideX,-sideY,  imgBackground2.width,imgBackground2.height);
+	
+  // top-right corner
+  // Map image pixel with (U,V) of (imgBackground.width,0) to top-right vertex of polygon.
+  vertex(sideX,sideY,  imgBackground2.width,0);
+
+  endShape(CLOSE);
+  wolfSprite.setPositionX(500);
+  wolfSprite.setPositionY(-50);
+  //wolfSprite.setScaleX(1.5);
+  //wolfSprite.setRotationZ(90);
+  //wolfSprite.applyTransforms();
+  wolfSprite.draw();
+  if(frameCounter % 16 == 0)
+    wolfSprite.update();
+}
+
+function scene3(){
+  
+  
+}
